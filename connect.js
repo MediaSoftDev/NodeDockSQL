6f4ac5a83e795cc9649a9c6b794a4a47a8d64363
Daniel Arturo Alejo Alvarez <18106359+DanielArturoAlejoAlvarez@users.noreply.github.com>
2025-02-05 11:21:15 -0500

    Create connect.js
    Connection with MySQL

---
+++ connect.js
@@ -0,0 +1,6 @@
+export const pool = createPool({
+    host: process.env.MYSQL_DB_HOST,
+    user: process.env.MYSQL_ROOT_USERNAME,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    port: process.env.MYSQL_DOCKER_PORT
+})
